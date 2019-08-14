21303a488c73b07e2358badef42ed9cbd952803e
Putra Prima Arhandi <putraprima@gmail.com>
2019-08-14 09:54:45 +0700

    tambah dependency dan routes hello world

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 "use strict";
 
 const Hapi = require("@hapi/hapi");
+const Mongoose = require("mongoose");
+const Joi = require("joi");
 
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
     host: "localhost"
   });
+  // routes hello wolrd
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (req, h) => {
+      return "hello world";
+    }
+  });
+
+  // routes profile
 
   await server.start();
   console.log("Server running on %s", server.info.uri);
